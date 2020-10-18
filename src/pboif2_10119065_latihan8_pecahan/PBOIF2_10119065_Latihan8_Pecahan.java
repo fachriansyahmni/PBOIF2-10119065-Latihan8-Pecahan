b884ec9b718417d8520e9321bc99da74b35d1065
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-18 14:56:56 +0700

    commit files

---
+++ src/pboif2_10119065_latihan8_pecahan/PBOIF2_10119065_Latihan8_Pecahan.java
@@ -0,0 +1,22 @@
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan bilangan pecahan 
+ *
+ */
+package pboif2_10119065_latihan8_pecahan;
+
+public class PBOIF2_10119065_Latihan8_Pecahan {
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 252.111235F;
+        d = 1_125_513_663.1234;
+        
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+    }
+    
+}
